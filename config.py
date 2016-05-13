efdd30c543e02ea066767259a1717ec52b834e26
Davide Mauri <info@davidemauri.it>
2016-05-13 16:27:22 +0200

    Fixed merge issue

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
 AZURE = False
 DEBUG = True
-<<<<<<< HEAD
-CONNECTION_STRING = "DRIVER={SQL Server};SERVER=localhost;DATABASE=SSISDB;UID=;PWD="
-HOUR_SPAN = 72
-=======
 CONNECTION_STRING = {
                     "main": "DRIVER={SQL Server};SERVER=localhost;DATABASE=SSISDB;UID=;PWD="
                     }
-HOUR_SPAN = 720
->>>>>>> refs/remotes/origin/development
+HOUR_SPAN = 72
