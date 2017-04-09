4a5c1a26e0a600406511f681474a1b9e3c425cc8
yorek <info@davidemauri.it>
2017-04-08 17:35:59 -0700

    fixed smart menu js

--- dashboard/query/engine-folders.sql
+++ dashboard/query/engine-folders.sql
@@ -1,1 +1,8 @@
-SELECT folder_id, name, [description] FROM [catalog].folders
+SELECT 
+	f.folder_id, 
+	f.[name], 
+	[description] 
+FROM 
+	[catalog].folders f
+WHERE 
+	EXISTS (SELECT * FROM [catalog].projects p WHERE p.folder_id = f.folder_id)
