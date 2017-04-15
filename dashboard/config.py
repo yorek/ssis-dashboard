d2bbdbdd86ee503068ef741720f33739139d4d42
yorek <info@davidemauri.it>
2017-04-14 23:55:40 -0700

    Updated config

--- dashboard/config.py
+++ dashboard/config.py
@@ -2,7 +2,6 @@ AZURE = False
 DEBUG = True
 CONNECTION_STRING = {
                     "main": "DRIVER={SQL Server};SERVER=localhost;DATABASE=SSISDB;UID=;PWD=",
-                    "main2": "DRIVER={SQL Server};SERVER=datarace.northeurope.cloudapp.azure.com,21433;DATABASE=SSISDB;UID=dmauri;PWD=Baldur_sgate4"
                     }
 HOUR_SPAN = 360
 PORT = 5555
