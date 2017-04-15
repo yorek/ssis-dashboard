7c01155aa1770d9501442138ebcc136d53439351
yorek <info@davidemauri.it>
2017-04-14 23:27:24 -0700

    Additional GUI updates

---
+++ dashboard/query/package-events-all.sql
@@ -0,0 +1,14 @@
+DECLARE @executionId BIGINT = ?;
+
+SELECT 
+	message_time,
+	[message],
+	package_name,
+	package_path,
+	subcomponent_name,
+	execution_path
+FROM 
+	[catalog].event_messages
+WHERE
+	operation_id = @executionId
+
