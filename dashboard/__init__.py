6a0cd49679419c18c08e0e072eaf91fafef94904
yorek <info@davidemauri.it>
2017-06-28 18:29:02 +0200

    Added support for configuration via environment variable

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(config)
+app.config.from_envvar('DASHBOARD_CONFIG')
 
 from dashboard.views import *
 from dashboard.processors import *
