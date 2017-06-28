0a2324521482b394462415a99467bcd85e575ace
yorek <info@davidemauri.it>
2017-06-28 19:17:23 +0200

    Allowed configuration via env variable OR config.cfg file

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -4,10 +4,22 @@ The flask application package.
 
 from dashboard import config
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
 app.config.from_object(config)
-app.config.from_envvar('DASHBOARD_CONFIG')
+
+envConfig = 'DASHBOARD_CONFIG'
+fileConfig = 'config.cfg'
+
+envConfigExists = os.getenv(envConfig) != None
+fileConfigExists = os.path.isfile('config.cfg')
+
+if envConfigExists == False and fileConfigExists == True:
+    os.environ[envConfig] = '..\\' + fileConfig
+
+app.config.from_envvar(envConfig)
 
 from dashboard.views import *
 from dashboard.processors import *
