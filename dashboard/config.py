cd5026b82af883be779b2c66c1ad362a3a795b69
yorek <info@davidemauri.it>
2018-02-11 20:32:05 -0800

    corrected connection string to work on linux

--- dashboard/config.py
+++ dashboard/config.py
@@ -1,9 +1,8 @@
 AZURE = False
 DEBUG = True
 CONNECTION_STRING = {
-                    "main": "DRIVER={ODBC Driver 13 for SQL Server};SERVER=localhost;DATABASE=SSISDB;UID=;PWD=",
+                    "main": "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=SSISDB;UID=;PWD=",
                     }
 HOUR_SPAN = 360
 AS_OF_DATE = 'NOW'
-PORT = 5555
 GOOGLE_ANALYTICS = None
